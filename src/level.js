1918632716c2459fe206e60606bf8834eb8ee1f4
jborowski <borowski.jedrzej@gmail.com>
2015-08-11 22:38:46 +0200

    moved things around a bit

--- src/level.js
+++ src/level.js
@@ -7,11 +7,11 @@ NSTC.Level.prototype = {
     this.fingers = this.game.add.group();
     this.targets = this.game.add.group();
 
-    this.oneFinger = new NSTC.Finger(this, 280, 400, 'a');
-    this.twoFinger = new NSTC.Finger(this, 350, 400, 's');
-    this.threeFinger = new NSTC.Finger(this, 420, 400, 'd');
+    this.oneFinger = new NSTC.Finger(this, 250, 400, 'a');
+    this.twoFinger = new NSTC.Finger(this, 380, 400, 's');
+    this.threeFinger = new NSTC.Finger(this, 510, 400, 'd');
 
-    this.oneTarget = new NSTC.Target(this, this.oneFinger, this.twoFinger, 2000, 150, 0);
+    this.oneTarget = new NSTC.Target(this, this.oneFinger, this.twoFinger, 4500, 315, 0);
 
     this.game.add.text(50, 50, this.definition.text, { fill: '#FFF' });
     this.loopTimerText = this.game.add.text(50, 150, '', { fill: '#FFF' });

a73452d784a8a7351c8ac78458b689d31aac8809
GlyphGryph <glyphgryph@gmail.com>
2015-08-14 22:20:21 -0400

    Fingers can now miss targets

--- src/target.js
+++ src/target.js
@@ -4,17 +4,19 @@ NSTC.Target = function(state, attachments){
   
   this.cVars = {
     positions: [],
-    active: false,
-    hitBy: null
+    hit: false,
+    passed: 0,
+    dead: false
   };
   this.attachments = attachments;
 
-  this.inactiveGraphic = new Phaser.Graphics().beginFill(0xFF0000).drawRect(0,0,50,50);
-  this.activeGraphic = new Phaser.Graphics().beginFill(0x00FF00).drawRect(0,0,50,50);
-  this.hitGraphic = new Phaser.Graphics().beginFill(0x888888).drawRect(0,0,50,50);
+  this.openGraphic = new Phaser.Graphics().beginFill(0xBB0022).drawRect(0,0,50,50);
+  this.deadGraphic = new Phaser.Graphics().beginFill(0x222222).drawRect(0,0,50,50);
+  this.passGraphic = new Phaser.Graphics().beginFill(0xFFFFFF).drawRect(0,0,50,50);
+  this.hitGraphic = new Phaser.Graphics().beginFill(0xFF3399).drawRect(0,0,50,50);
   var firstLine = this.attachments[0].finger.line;
   var firstPoint = firstLine.getPointAt(this.attachments[0].position);
-  Phaser.Sprite.call(this, this.game, firstPoint.x, firstPoint.y+50, this.inactiveGraphic.generateTexture());
+  Phaser.Sprite.call(this, this.game, firstPoint.x, firstPoint.y+50, this.openGraphic.generateTexture());
   this.state.targets.add(this);
   this.anchor.setTo(0,0.5);
   this.box = new Phaser.Rectangle(firstPoint.x, firstPoint.y+40, 50, 20);
@@ -25,6 +27,21 @@ NSTC.Target = function(state, attachments){
       var attachment = this.attachments[ii];
       if(Phaser.Rectangle.containsPoint(this.box, attachment.finger)){
         attachment.finger.onTarget();
+        this.cVars.hit = true;
+        this.texture = this.hitGraphic.generateTexture();
+      }
+      if(!attachment.passed && this.box.right < attachment.finger.x){
+        this.cVars.passed +=1;
+        attachment.passed = true;
+      }
+    }
+    if(!this.cVars.dead && this.cVars.passed >= this.attachments.length){
+      this.cVars.dead = true;
+      if(this.cVars.hit){
+        this.texture = this.passGraphic.generateTexture();
+      } else {
+        this.state.score -= 100;
+        this.texture = this.deadGraphic.generateTexture();
       }
     }
   };
